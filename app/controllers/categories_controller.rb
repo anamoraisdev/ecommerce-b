02c3f8b1f45ef6b68667abc11552808a9cc62ee5
Ana Karolina Morais <anakarolinamorais@MacBook-Air-de-Ana.local>
2024-03-19 15:27:35 -0300

    correting

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,28 +9,6 @@ class CategoriesController < ApplicationController
         return render json: @category
     end
 
-    def new 
-        @category = Category.new(categories_params)
-    end
-
-    def create
-        @category = Category.new(categories_params)
-        @category.save
-        return render json: @category
-    end 
-
-    def destroy
-        @category = Category.find(params[:id])
-        @category.destroy
-        return render json: Category.all
-    end
-
-    def update
-        @category = Category.find(params[:id])
-        @category.update(categories_params)
-        return render json: @category
-    end
-
     private
     def categories_params
       params.require(:category).permit(:name)
